c6d5a1b76975c65d2dec53f2ba938e3185057195
Kyle Galway <kyligalway@gmail.com>
2023-05-08 12:06:45 -0400

    Initial commit.

---
+++ Module1/1_HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1_HelloWorld
+{
+    internal class Program
+    {
+        // The main method in C# uses a capital Main. Strings in C# are a primitive type by default, not a class. 
+        static void Main(string[] args)
+        {
+            // Console is the class for operating on the command line. 
+            Console.WriteLine("Hello world!");
+            Console.Write("Write something: ");
+            int intInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have entered: " + intInput);
+
+        }
+    }
+}
+
+// Define 
+// Public: 
+// Private: 
+// Protected: 
+// Internal:
+// Default: Used to define a default body for an interface. (Research Further). 
+
+// C# doesn't use packages, but instead just uses folders without the nomenclature. 
+
